629f22579b9f72afb2a98ebb5860e8c529134be3
ImOnStalking <Kazukuzin21@gmail.com>
2025-11-15 21:54:20 +0800

    Update
    Added js,css and fixed navbar problems

--- JS/script.js
+++ JS/script.js
@@ -14,4 +14,26 @@ button.addEventListener("click", function() {
     }
 });
 
+//welcome alert
+window.addEventListener("load", function() {
+
+    alert("Welcome to Movie Sphere! Enjoy exploring movies.");
+});
+
+//login modal functionality
+var loginButton = document.querySelector("#loginModal button[type='submit']");
+if (loginButton) {
+    loginButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        var emailInput = document.querySelector("#email");
+        var passwordInput = document.querySelector("#password");
+        if (emailInput.value && passwordInput.value) {
+            alert("Login successful! Welcome back "+(emailInput.value));
+            window.location.href = "index.html";
+        } else {
+            alert("Please fill in all fields.");
+        }
+    });
+}
+
 
